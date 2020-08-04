fa69d24b752ea694a185804b6bd2b10a0711a42b
Leandro Iamele <leandroiamele@gmail.com>
2020-08-04 11:30:22 -0300

    Add validations to Restaurant and Review models

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,6 @@
 class Restaurant < ActiveRecord::Base
   has_many :reviews
+  validates :name, :address, presence: true
+  CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
+  validates :category, inclusion: {in: CATEGORIES }
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,5 @@
 class Review < ActiveRecord::Base
-  belongs_to :restaurant
+  belongs_to :restaurant, dependent: :destroy
+  validates :rating, presence: true, inclusion: { in: 1..5 }, numericality: { only_integer: true }
+  validates :content, presence: true
 end
